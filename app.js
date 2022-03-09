470dc070385ff269acb157f26c7c50008c4a23bc
iheeb9 <iheb.youssef@esprit.tn>
2022-03-09 10:26:33 +0100

    firstUser

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cors =require('cors')
 
 
 var app = express();
-
+  
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -19,10 +19,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use ('/api',require('./routes/authRouter'));
-// app.use ('/api',require('./routes/commentRouter'));
+app.use ('/api',require('./routes/authRouter'));
+app.use ('/api',require('./routes/commentRouter'));
 
-// app.use ('/api',require('./routes/postRouter'));
+app.use ('/api',require('./routes/postRouter'));
 app.use(cors);
 
 // catch 404 and forward to error handler
