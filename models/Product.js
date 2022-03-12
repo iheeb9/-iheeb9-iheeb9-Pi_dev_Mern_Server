c1634766b4b23fb557d8880748b0dbef8b880c71
Majdi-msallem <majdi.msallem@esprit.tn>
2022-03-12 14:43:14 +0100

    update product model

--- models/Product.js
+++ models/Product.js
@@ -4,10 +4,27 @@ const productSchema = new mongoose.Schema(
       name: { type: String },
       image: { type: String },
       brand: { type: String},
-      category: { type: String},
+      category: {
+         type: String,
+         enum:[
+           'Electronics',
+           'Cameras',
+           'Laptop',
+           'Accessories',
+           'Headphones', 
+           'Food',
+           'Books',
+           'Clothes/shoes',
+           'Beauty/Health',
+           'Sports',
+           'Outdoor',
+           'Other',
+         ],
+          message :'Please select correct category for correct product'
+        },
       description: { type: String},
-      price: { type: Number},
-
+      price: { type: Number,default: 0},
+      countInStock: {type: String ,default: 0},
       cloudinary_id:{
         type:String,
       },
